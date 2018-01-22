fc841616c79224a26d8d288f8aadb78ea4c9e6ce
robotemperor <leeche12@hanyang.ac.kr>
2018-01-22 16:42:52 +0900

    shell modified

--- command_real.sh
+++ command_real.sh
@@ -3,10 +3,6 @@
 source /opt/ros/kinetic/setup.bash
 source ~/catkin_ws/devel/setup.bash
 
-roscore &
-
-sleep 5
-
 rosrun diana_sensors ft_node &
 
 roslaunch microstrain_3dm_gx5_45 microstrain.launch&
